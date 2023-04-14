d525422b4ce32413739780af802f05f39373163b
WidharDwiatmoko <widhardwi23@gmail.com>
2023-04-14 20:07:21 +0700

    Fix Bulk API

--- router.py
+++ router.py
@@ -1,7 +1,7 @@
 # Import the Libraries
 import numpy as np
 import pandas as pd
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, jsonify
 import joblib
 import os
 # the function I craeted to process the data in utils.py
@@ -23,6 +23,46 @@ model = util.pickle_load(params["production_model_path"])
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+# Route for API Call predict
+@app.route('/predict_api', methods=['POST'])
+def predict_api():
+    data_get = request.get_json(force=True)
+    data = data_get["data"]
+    # print(data["long"])
+    # return jsonify(data["long"])
+    long = data['long']
+    latit = data['lat']
+    med_age = data['med_age']
+    total_rooms = data['total_rooms']
+    total_bedrooms = data['total_bedrooms']
+    pop = data['pop']
+    hold = data['hold']
+    income = data['income']
+    ocean = data['ocean']
+
+    # return jsonify(ocean)
+
+    # # Remmber the Feature Engineering we did
+    rooms_per_hold = total_rooms / hold
+    bedroms_per_rooms = total_bedrooms / total_rooms
+    pop_per_hold = pop / hold
+
+    # # Concatenate all Inputs
+    X_new = pd.DataFrame({'longitude': [long], 'latitude': [latit], 'housing_median_age': [med_age], 'total_rooms': [total_rooms],
+                          'total_bedrooms': [total_bedrooms], 'population': [pop], 'households': [hold], 'median_income': [income],
+                          'ocean_proximity': [ocean], 'rooms_per_household': [rooms_per_hold], 'bedroms_per_rooms': bedroms_per_rooms,
+                          'population_per_household': [pop_per_hold]
+                          })
+
+    # # Call the Function and Preprocess the New Instances
+    X_processed = preprocess_new(X_new)
+
+    # # call the Model and predict
+    y_pred_new = model.predict(X_processed)
+    y_pred_new = '{:.4f}'.format(y_pred_new[0])
+    return jsonify(y_pred_new)
 
 # Route for Predict page
 
